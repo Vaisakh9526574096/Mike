26cf2e681752fe3f129ce8113f0b1ebb2d499bb2
MikeTG000 <119830644+MikeTG000@users.noreply.github.com>
2022-12-07 20:06:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1163,7 +1163,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.FILE_TXT,
+            text=script.BATCH_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
